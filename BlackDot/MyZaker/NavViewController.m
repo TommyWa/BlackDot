ec59ffce2e747cf6a12193bfd3fdd8eeb58918bf
woyoto <woyoto@qq.com>
2016-08-29 11:48:36 +0800

    删除登录界面相关操作

--- BlackDot/MyZaker/NavViewController.m
+++ BlackDot/MyZaker/NavViewController.m
@@ -10,7 +10,7 @@
 #import "FactoryUtil.h"
 #import "ProgressHUD.h"
 #import "MJRefresh.h"
-#import "LoginViewController.h"
+
 @interface NavViewController ()
 @property(nonatomic,strong)UITableView * tableView;
 @end
@@ -31,22 +31,14 @@
 
 //自定义导航栏内容
 - (void)setMyNavgationItem{
-    
-    UIButton * button = [FactoryUtil createButtonWithTitle:nil frame:CGRectMake(0, 0, 30, 30) backImage:[UIImage imageNamed:@"ic_account_circle_48px"] tatget:self action:@selector(gotoHomepage)];
-    self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView:button];
-    self.navigationController.navigationBar.barTintColor = methemColor;
+//    
+//    UIButton * button = [FactoryUtil createButtonWithTitle:nil frame:CGRectMake(0, 0, 30, 30) backImage:[UIImage imageNamed:@"ic_account_circle_48px"] tatget:self action:@selector(gotoHomepage)];
+//    self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView:button];
+//    self.navigationController.navigationBar.barTintColor = methemColor;
     
 }
 
 #pragma Mark 进入个人设置界面，未实现
-- (void)gotoHomepage{
-    LoginViewController * vc = [[LoginViewController alloc] init];
-    vc.view.backgroundColor = [UIColor cyanColor];
-    self.hidesBottomBarWhenPushed = YES;
-    [self.navigationController pushViewController:vc animated:YES];
-    self.hidesBottomBarWhenPushed = NO;
-
-}
 
 
 
